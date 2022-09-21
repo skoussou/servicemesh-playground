2d6d4881a5409d5d7cafb54d7ff03c19fd69eaa9
skoussou <skousou@gmail.com>
2022-09-21 09:16:21 +0100

    Scenario-MTLS-3 - 1a greetings service side docs

--- Scenario-MTLS-3-SM-Service-To-External-MTLS-Handling/create-greeting-client-1b-encrypted.sh
+++ Scenario-MTLS-3-SM-Service-To-External-MTLS-Handling/create-greeting-client-1b-encrypted.sh
@@ -139,10 +139,10 @@ spec:
             - >-
               /deployments/lib/jakarta.annotation.jakarta.annotation-api-1.3.5.jar:/deployments/lib/jakarta.el.jakarta.el-api-3.0.3.jar:/deployments/lib/jakarta.interceptor.jakarta.interceptor-api-1.2.5.jar:/deployments/lib/jakarta.enterprise.jakarta.enterprise.cdi-api-2.0.2.jar:/deployments/lib/jakarta.inject.jakarta.inject-api-1.0.jar:/deployments/lib/io.quarkus.quarkus-development-mode-spi-1.12.0.Final.jar:/deployments/lib/io.smallrye.common.smallrye-common-annotation-1.5.0.jar:/deployments/lib/io.smallrye.config.smallrye-config-common-1.10.2.jar:/deployments/lib/io.smallrye.common.smallrye-common-function-1.5.0.jar:/deployments/lib/io.smallrye.common.smallrye-common-expression-1.5.0.jar:/deployments/lib/io.smallrye.common.smallrye-common-constraint-1.5.0.jar:/deployments/lib/io.smallrye.common.smallrye-common-classloader-1.5.0.jar:/deployments/lib/io.smallrye.config.smallrye-config-1.10.2.jar:/deployments/lib/org.jboss.logging.jboss-logging-3.4.1.Final.jar:/deployments/lib/org.jboss.logmanager.jboss-logmanager-embedded-1.0.6.jar:/deployments/lib/org.jboss.logging.jboss-logging-annotations-2.2.0.Final.jar:/deployments/lib/org.jboss.threads.jboss-threads-3.2.0.Final.jar:/deployments/lib/org.slf4j.slf4j-api-1.7.30.jar:/deployments/lib/org.jboss.slf4j.slf4j-jboss-logmanager-1.1.0.Final.jar:/deployments/lib/org.graalvm.sdk.graal-sdk-21.0.0.jar:/deployments/lib/org.wildfly.common.wildfly-common-1.5.4.Final-format-001.jar:/deployments/lib/io.smallrye.common.smallrye-common-io-1.5.0.jar:/deployments/lib/io.quarkus.quarkus-bootstrap-runner-1.12.0.Final.jar:/deployments/lib/io.quarkus.quarkus-core-1.12.0.Final.jar:/deployments/lib/io.quarkus.quarkus-security-runtime-spi-1.12.0.Final.jar:/deployments/lib/jakarta.transaction.jakarta.transaction-api-1.3.3.jar:/deployments/lib/io.quarkus.arc.arc-1.12.0.Final.jar:/deployments/lib/io.quarkus.quarkus-vertx-http-dev-console-runtime-spi-1.12.0.Final.jar:/deployments/lib/org.reactivestreams.reactive-streams-1.0.3.jar:/deployments/lib/io.smallrye.reactive.mutiny-0.13.0.jar:/deployments/lib/io.quarkus.security.quarkus-security-1.1.3.Final.jar:/deployments/lib/io.netty.netty-codec-4.1.49.Final.jar:/deployments/lib/io.netty.netty-handler-4.1.49.Final.jar:/deployments/lib/io.quarkus.quarkus-netty-1.12.0.Final.jar:/deployments/lib/io.netty.netty-common-4.1.49.Final.jar:/deployments/lib/io.netty.netty-buffer-4.1.49.Final.jar:/deployments/lib/io.netty.netty-transport-4.1.49.Final.jar:/deployments/lib/io.netty.netty-codec-socks-4.1.49.Final.jar:/deployments/lib/io.netty.netty-handler-proxy-4.1.49.Final.jar:/deployments/lib/io.netty.netty-codec-http-4.1.49.Final.jar:/deployments/lib/io.netty.netty-codec-http2-4.1.49.Final.jar:/deployments/lib/io.netty.netty-resolver-4.1.49.Final.jar:/deployments/lib/io.netty.netty-codec-dns-4.1.49.Final.jar:/deployments/lib/io.netty.netty-resolver-dns-4.1.49.Final.jar:/deployments/lib/com.fasterxml.jackson.core.jackson-core-2.12.1.jar:/deployments/lib/io.vertx.vertx-core-3.9.5.jar:/deployments/lib/io.quarkus.quarkus-vertx-core-1.12.0.Final.jar:/deployments/lib/io.vertx.vertx-web-common-3.9.5.jar:/deployments/lib/io.vertx.vertx-auth-common-3.9.5.jar:/deployments/lib/io.vertx.vertx-bridge-common-3.9.5.jar:/deployments/lib/io.vertx.vertx-web-3.9.5.jar:/deployments/lib/io.quarkus.quarkus-vertx-http-1.12.0.Final.jar:/deployments/lib/org.eclipse.microprofile.context-propagation.microprofile-context-propagation-api-1.0.1.jar:/deployments/lib/io.quarkus.quarkus-arc-1.12.0.Final.jar:/deployments/lib/org.jboss.spec.javax.ws.rs.jboss-jaxrs-api_2.1_spec-2.0.1.Final.jar:/deployments/lib/org.jboss.spec.javax.xml.bind.jboss-jaxb-api_2.3_spec-2.0.0.Final.jar:/deployments/lib/org.jboss.resteasy.resteasy-core-spi-4.5.9.Final.jar:/deployments/lib/com.ibm.async.asyncutil-0.1.0.jar:/deployments/lib/org.eclipse.microprofile.config.microprofile-config-api-1.4.jar:/deployments/lib/org.jboss.resteasy.resteasy-core-4.5.9.Final.jar:/deployments/lib/com.sun.activation.jakarta.activation-1.2.1.jar:/deployments/lib/io.quarkus.quarkus-resteasy-common-1.12.0.Final.jar:/deployments/lib/jakarta.validation.jakarta.validation-api-2.0.2.jar:/deployments/lib/io.quarkus.quarkus-resteasy-server-common-1.12.0.Final.jar:/deployments/lib/io.quarkus.quarkus-resteasy-1.12.0.Final.jar:/deployments/lib/io.quarkus.quarkus-kubernetes-client-internal-1.12.0.Final.jar:/deployments/lib/io.quarkus.quarkus-openshift-1.12.0.Final.jar
             - '-Dquarkus.http.host=0.0.0.0'
-            - '-Djava.util.logging.manager=org.jboss.logmanager.LogManager' "|oc apply -f - 
+            - '-Djava.util.logging.manager=org.jboss.logmanager.LogManager' " |oc apply -f -
    
    
-   
+
 echo
 echo "################# Service - rest-client-greeting [$SM_MR_NS] #################"   
 echo 
@@ -213,7 +213,7 @@ echo "##########################################################################
 echo "#		OUTGOING TRAFFIC SM CONFIGS                                        #"
 echo "#############################################################################"
 echo           
-echo "################# ServiceEntry - rest-greeting-remote-mesh-ext [$SM_MR_NS] #################"    
+echo "################# ServiceEntry - rest-greeting-remote-mesh-ext [$SM_MR_NS] #################"  
 echo "kind: ServiceEntry
 apiVersion: networking.istio.io/v1alpha3
 metadata:
@@ -222,7 +222,23 @@ metadata:
 spec:
   hosts:
     - ${REMOTE_SERVICE_ROUTE_NAME}
-  addresses: ~
+  ports:
+    - name: http
+      number: 443
+      protocol: HTTP2
+  location: MESH_EXTERNAL
+  resolution: DNS
+  exportTo:
+  - '*'" 
+  
+echo "kind: ServiceEntry
+apiVersion: networking.istio.io/v1alpha3
+metadata:
+  name: rest-greeting-remote-mesh-ext
+  namespace: $SM_MR_NS
+spec:
+  hosts:
+    - ${REMOTE_SERVICE_ROUTE_NAME}
   ports:
     - name: http
       number: 443
@@ -260,7 +276,7 @@ spec:
   
 echo  
 # 2. destination rules and virtual services to direct the traffic through the egress gateway and from the egress gateway to the external service.       
-echo "################# DestinationRule - egress-originate-tls-to-rest-greeting-remote-destination-rule [$SM_MR_NS] #################"    
+echo "################# DestinationRule - egress-originate-tls-to-rest-greeting-remote-destination-rule [$SM_MR_NS] #################" 
 echo "kind: DestinationRule
 apiVersion: networking.istio.io/v1alpha3
 metadata:
@@ -268,7 +284,26 @@ metadata:
   namespace: $SM_MR_NS  
 spec:
   host: istio-egressgateway.${SM_CP_NS}.svc.cluster.local
-  trafficPolicy:
+  subsets:
+  - name: greeting-remote
+    trafficPolicy:
+      loadBalancer:
+        simple: ROUND_ROBIN
+      portLevelSettings:
+      - port:
+          number: 443
+        tls:
+          mode: ISTIO_MUTUAL
+          sni: ${REMOTE_SERVICE_ROUTE_NAME}
+  exportTo:
+  - '.'"   
+echo "kind: DestinationRule
+apiVersion: networking.istio.io/v1alpha3
+metadata:
+  name: egress-originate-tls-to-rest-greeting-remote
+  namespace: $SM_MR_NS  
+spec:
+  host: istio-egressgateway.${SM_CP_NS}.svc.cluster.local
   subsets:
   - name: greeting-remote
     trafficPolicy:
